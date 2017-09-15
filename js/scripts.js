62eaf8bb5776f3d7242429bb2f8ccf76d6360c59
Byron Blocker <byronblocker@gmail.com>
2017-09-15 09:39:10 -0700

    add global variables for radio button values and language points

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
 $(document).ready(function() {
     $('form#questions').submit(function(event) {
-        $('.jumbotron').fadeOut();
         event.preventDefault();
+        // $('.jumbotron').fadeOut();
+        // $('.cSharp').fadeIn();
+        var answer1 = $('input:radio[name=q1]:checked').val();
+        var answer2 = $('input:radio[name=q2]:checked').val();
+        var answer3 = $('input:radio[name=q3]:checked').val();
+        var answer4 = $('input:radio[name=q4]:checked').val();
+        var answer5 = $('input:radio[name=q5]:checked').val();
+        var answer6 = $('input:radio[name=q6]:checked').val();
+
+        var cSharpPts = 0;
+        var javaPts = 0;
+        var phpPts = 0;
+        var rubyPts = 0;
+
+
 
     });
 });
