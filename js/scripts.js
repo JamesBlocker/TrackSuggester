1f252fb7811f689fb2aa62147d89ed9b67d61dfe
Byron Blocker <byronblocker@gmail.com>
2017-09-15 09:33:11 -0700

    update js with event prevent and basic submit functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
+    $('form#questions').submit(function(event) {
+        $('.jumbotron').fadeOut();
+        event.preventDefault();
 
-    // $('.jumbotron').fadeOut();
-
+    });
 });
